67b8081cc3fae65c18a3ed313d0a9c655f4b14dd
arnog <arno@arno.org>
2021-03-30 10:47:31 +0200

    chore: deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,31 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        // Uses the recommended rules for Typescript
-        'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-        // Disable rules that conflict with prettier
-        // See https://prettier.io/docs/en/integrating-with-linters.html
-        'plugin:prettier/recommended',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    // Uses the recommended rules for Typescript
+    'plugin:@typescript-eslint/recommended',
+    // Disable rules that conflict with prettier
+    // See https://prettier.io/docs/en/integrating-with-linters.html
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        // "vars": "all",
+        // "varsIgnorePattern": "^_",
+        // "args": "after-used",
+        argsIgnorePattern: '^_',
+        // "ignoreRestSiblings": false
+      },
     ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint'],
-    rules: {
-        '@typescript-eslint/no-unused-vars': [
-            'error',
-            {
-                // "vars": "all",
-                // "varsIgnorePattern": "^_",
-                // "args": "after-used",
-                argsIgnorePattern: '^_',
-                // "ignoreRestSiblings": false
-            },
-        ],
-    },
+  },
 };
